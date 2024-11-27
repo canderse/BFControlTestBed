a4031562f1b23f4805fa311c3564c131b5f0b772
christian Andersen <candese@dedata.com.au>
2024-11-28 09:52:36 +1100

    cleand up check box

--- BFControlTestBed/BFControlTestBed/ViewController.swift
+++ BFControlTestBed/BFControlTestBed/ViewController.swift
@@ -12,8 +12,14 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        chackBox.checkboxUnknowenType = .unknownCircle
     }
 
-
+    @IBOutlet weak var chackBox: BFCheckBox!
+   
+    @IBAction func chackBoxValueChanged(_ sender: Any) {
+    }
+    
+    
 }
 
