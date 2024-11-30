a50fdeb834474525f5a89935aad4151c2e8f56c6
chrstian Andersen <canderse@dedata.com.au>
2024-11-30 14:46:01 +1100

    added more stuff to keyboard

--- BFControlTestBed/BFControlTestBed/Controls/BFNumericField.swift
+++ BFControlTestBed/BFControlTestBed/Controls/BFNumericField.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AudioToolbox
 
 public enum BFKey : Int
 {
@@ -30,10 +31,7 @@ public enum BFKey : Int
 @IBDesignable class BFNumericKeyPad : UIInputView
 {
     
-    
-    
-    
-    
+
     @IBOutlet weak var zeroButton: UIButton!
     
     @IBOutlet weak var oneButton: UIButton!
@@ -92,6 +90,9 @@ public enum BFKey : Int
     
     
     
+    
+    
+    
     static public func Create(ownerEditor : BFNumericField )
     {
         let keyPad = Bundle.main.loadNibNamed("BFNumericKeyPad-Standard", owner: ownerEditor, options: .none)?[0] as? BFNumericKeyPad
@@ -103,7 +104,15 @@ public enum BFKey : Int
             ownerEditor.inputAssistantItem.allowsHidingShortcuts = true
             ownerEditor.inputAssistantItem.leadingBarButtonGroups = []
             ownerEditor.inputAssistantItem.trailingBarButtonGroups = []
+            keyPad?.decimalPointButton.isHidden = !ownerEditor.isDecimal
+            keyPad?.invertSignButton.isHidden = !ownerEditor.signButtonVisible
         }
+    }
+    
+    
+    private func playSoundWithId(id : Int)
+    {
+        
     }
     
     @IBAction func buttonTouched(_ sender: UIButton)
@@ -143,17 +152,21 @@ public enum BFKey : Int
         case clearButton:
             _ownerEditor?.processKey(key: BFKey.zero)
             break
+        case backButton:
+            _ownerEditor?.processKey(key: BFKey.backSpace)
+            break;
+        case decimalPointButton:
+            _ownerEditor?.processKey(key: BFKey.decimalPoint)
+            break;
+        case invertSignButton:
+            _ownerEditor?.processKey(key: BFKey.invertSign)
+            break;
+        case closeKeyboardButton:
+            _ownerEditor?.processKey(key: BFKey.closeKeyboard)
         default:
             break
         }
-        
-            
-        
     }
-    
-    
-    
-    
 }
 
 @IBDesignable public class BFNumericField: UITextField,UITextFieldDelegate
@@ -184,10 +197,23 @@ public enum BFKey : Int
         setUp()
     }
     
+    private var _localDelegate : (any UITextFieldDelegate)? = nil
+    
+    override public var delegate: (any UITextFieldDelegate)?
+    {
+        get
+        {
+            return _localDelegate
+        }
+        set(newValue)
+        {
+            _localDelegate = newValue
+        }
+    }
     
     private func setUp()
     {
-        self.delegate = self
+        super.delegate = self
         
     }
     
@@ -195,16 +221,136 @@ public enum BFKey : Int
     public func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
         
         BFNumericKeyPad.Create(ownerEditor:  self)
+        if(_localDelegate?.responds(to: #selector(UITextFieldDelegate.textFieldShouldBeginEditing(_:))) ?? false)
+        {
+            return _localDelegate!.textFieldShouldEndEditing!(textField)
+        }
         return true;
     }
     
-    @IBInspectable public var isDecimal : Bool = false
+    public func textFieldDidBeginEditing(_ textField: UITextField) {
+        if(_localDelegate?.responds(to: #selector(UITextFieldDelegate.textFieldDidBeginEditing(_:))) ?? false)
+        {
+            _localDelegate!.textFieldDidBeginEditing!(textField)
+        }
+    }
     
+    public func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if(_localDelegate?.responds(to: #selector(UITextFieldDelegate.textFieldShouldEndEditing(_:))) ?? false)
+        {
+            return _localDelegate!.textFieldShouldEndEditing!(textField)
+        }
+        return true
+    }
+    
+    
+    public func textFieldDidEndEditing(_ textField: UITextField) {
+        if(_localDelegate?.responds(to: #selector(UITextFieldDelegate.textFieldDidEndEditing(_:))) ?? false)
+        {
+             _localDelegate!.textFieldDidEndEditing!(textField)
+        }
+    }
+    
+    public func textFieldDidChangeSelection(_ textField: UITextField) {
+        if(_localDelegate?.responds(to: #selector(UITextFieldDelegate.textFieldDidChangeSelection(_:))) ?? false)
+        {
+             _localDelegate!.textFieldDidChangeSelection!(textField)
+        }
+    }
+    
+    public func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        if(_localDelegate?.responds(to: #selector(UITextFieldDelegate.textFieldShouldClear(_:))) ?? false)
+        {
+             return _localDelegate!.textFieldShouldClear!(textField)
+        }
+        return true
+    }
+    
+    public func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if(_localDelegate?.responds(to: #selector(UITextFieldDelegate.textFieldShouldReturn(_:))) ?? false)
+        {
+             return _localDelegate!.textFieldShouldReturn!(textField)
+        }
+        return true
+    }
+    
+     public func textFieldDidEndEditing(_ textField: UITextField, reason: UITextField.DidEndEditingReason) {
+        if(_localDelegate?.responds(to: #selector(UITextFieldDelegate.textFieldDidEndEditing(_:))) ?? false)
+        {
+            _localDelegate!.textFieldDidEndEditing!(textField)
+        }
+    }
+    
+     public func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+         if(_localDelegate?.responds(to: #selector(UITextFieldDelegate.textField(_:shouldChangeCharactersIn:replacementString:))) ?? false)
+         {
+             return _localDelegate!.textField!(textField, shouldChangeCharactersIn: range, replacementString: string)
+         }
+         return true;
+    }
+    
+    public func textField(_ textField: UITextField, editMenuForCharactersIn range: NSRange, suggestedActions: [UIMenuElement]) -> UIMenu? {
+        if #available(iOS 16.0, *) {
+            if(_localDelegate?.responds(to: #selector(UITextFieldDelegate.textField(_:editMenuForCharactersIn:suggestedActions:))) ?? false)
+            {
+                return _localDelegate!.textField!(textField, editMenuForCharactersIn: range, suggestedActions: suggestedActions )
+            }
+        }
+        return nil
+    }
+    
+    
+    @available(iOS 16.0, *)
+    public func textField(_ textField: UITextField, willDismissEditMenuWith animator: any UIEditMenuInteractionAnimating) {
+         if(_localDelegate?.responds(to: #selector(UITextFieldDelegate.textField(_:willDismissEditMenuWith:))) ?? false)
+            {
+                _localDelegate!.textField!(textField, willDismissEditMenuWith: animator)
+            }
+    }
+    
+    @available(iOS 16.0, *)
+    public func textField(_ textField: UITextField, willPresentEditMenuWith animator: any UIEditMenuInteractionAnimating) {
+        if(_localDelegate?.responds(to: #selector(UITextFieldDelegate.textField(_:willPresentEditMenuWith:))) ?? false)
+           {
+                _localDelegate!.textField!(textField, willPresentEditMenuWith: animator)
+           }
+    }
+
+    
+    @IBInspectable public var isDecimal : Bool = false
+    @IBInspectable public var signButtonVisible : Bool = false
     
     public func processKey(key : BFKey)
     {
-        
+        if(key.rawValue >= 0)
+        {
+          
+            let newChar = Character(UnicodeScalar( "0".unicodeScalars.first!.value + UInt32(key.rawValue))!)
+            if(_localDelegate?.responds(to: #selector(UITextFieldDelegate.textField(_:shouldChangeCharactersIn:replacementString:))) ?? false)
+            {
+               if(_localDelegate!.textField!(self, shouldChangeCharactersIn: NSRange(location: 0,length: text!.count), replacementString: String(newChar)))
+               {
+                   addCharacterToValue(newChar)
+               }
+            } else
+            {
+                addCharacterToValue(newChar)
+            }
+        } else
+        {
+            if(key == BFKey.decimalPoint && isDecimal)
+            {
+                if(!text!.contains("."))
+                {
+                    addCharacterToValue(".")
+                }
+            }
+        }
     }
     
-
+    private func addCharacterToValue(_ newChar : Character)
+    {
+        text = text! + String(newChar)  
+    }
+    
 }
