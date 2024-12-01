54f474cda1a8c53833b1d2c848f5aed82a3bfb22
chrstian Andersen <canderse@dedata.com.au>
2024-12-01 21:12:23 +1100

    added buttons images

---
+++ BFControlTestBed/BFControlTestBed/Controls/BFEnhancedNumberField.swift
@@ -0,0 +1,20 @@
+//
+//  BFEnhancedNumberField.swift
+//  BFControlTestBed
+//
+//  Created by Christian Andersen on 1/12/2024.
+//
+
+import UIKit
+
+class BFEnhancedNumberField: UIControl {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
